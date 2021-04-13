ee94e6a48ebe21151fe30e5e624ddf456f05f391
edrix77 <edrico.535170031@stu.untar.ac.id>
2021-04-13 11:11:59 +0700

    Tahap database dan awalan konfigurasi model

--- perpus/settings.py
+++ perpus/settings.py
@@ -77,8 +77,14 @@ WSGI_APPLICATION = 'perpus.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        # 'NAME' : os.path.join(BASE_DIR,'db.sqlite3'),
+        'NAME' : 'perpus',
+        'USER' : 'root',
+        'PASSWORD' : '123456',
+        'HOST' : 'localhost',
+        'PORT' : '3306',
     }
 }
 
